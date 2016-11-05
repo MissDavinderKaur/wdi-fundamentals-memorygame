2c4bb4917cd160222bb134762ec4dec81b8dd1cd
Davinder Kaur <miss_davinder_kaur@hotmail.co.uk>
2016-11-05 09:20:54 +0000

    Added conditional logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,4 +3,13 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+
+if (cardTwo === cardFour || cardTwo === cardThree)
+{
+	alert("Sorry, try again");
+}
+else if (cardTwo === cardOne)
+{
+	alert("You have found a match!")
+}
 //console.log("JS file is connected to HTML! Woo!")
